815a22646b9001727ac324f3529b993cf8b06e98
vasavitaneja20 <vasavitaneja@gmail.com>
2025-06-28 14:20:51 +0530

    user creation corrected

--- lms-server/controllers/webhooks.js
+++ lms-server/controllers/webhooks.js
@@ -27,7 +27,7 @@ export const clerkWebhooks = async(req, res) =>{
         case 'user.created':{
             const userData = {
                 _id: data.id,
-                email: data.email_address[0],
+                email: data.email_addresses[0].email_address,
                 name: data.first_name + " " + data.last_name,
                 imageUrl : data.image_url,
             }

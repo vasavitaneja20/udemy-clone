fd86748a8aae59badb224faf1378be6ad7b952b8
vasavitaneja20 <vasavitaneja@gmail.com>
2025-06-28 14:31:48 +0530

    webhooks corrected

--- lms-server/controllers/webhooks.js
+++ lms-server/controllers/webhooks.js
@@ -1,67 +1,50 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
-import { request } from "express";
 
-/*What Are Webhooks?. What is a Webhook? 
-A webhook is a way for one application to automatically send real-time data to another 
-application when a specific event occurs. It's essentially an automated message sent over 
-HTTP, triggered by an event, and delivered to a pre-defined URL. 
+//API controller function to manage Clerk user with database
+export const clerkWebhooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-This code sets up a webhook handler using Svix, which is a service for securely 
-verifying webhooks (in this case, from Clerk, an authentication provider).*/
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
 
+    const { data, type } = req.body;
 
-//API controller function to manage clerk user with database
-export const clerkWebhooks = async(req, res) =>{
-   try{
-       const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          imageUrl: data.image_url,
+        };
+        await User.create(userData);
+        return res.json({});
+      }
 
-       await whook.verify(JSON.stringify(req.body), {
-           "svix-id" : req.headers["svix-id"],
-           "svix-timestamp":  req.headers["svix-timestamp"],
-           "svix-signature" : req.header["svix-signature"]
-       })
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          imageUrl: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        return res.json({});
+      }
 
-       const {data, type} = req.body 
-       switch (type) {
-        case 'user.created':{
-            const userData = {
-                _id: data.id,
-                email: data.email_addresses[0].email_address,
-                name: data.first_name + " " + data.last_name,
-                imageUrl : data.image_url,
-            }
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        return res.json({});
+      }
 
-            //save data in database
-            await User.create(userData)
-            res.JSON({})
-            break;
-        }
-         
-        case 'user.updated': {
-            const userData = {
-                //id cannot be updated
-                email: data.email_address[0],
-                name: data.first_name + " " + data.last_name,
-                imageUrl : data.image_url,
-            }
-            await User.findByIdAndUpdate(data.id, userData)
-            res.JSON({})
-            break;
-        }
-
-        case 'user.deleted' : {
-            await User.findByIdAndDelete(data.id)
-            res.JSON({})
-            break;
-        }
-        
-       
-        default:
-            break;
-       }
-   }
-   catch(error){
-        res.JSON({success: 'false', message: error.message})
-   }
-}
+      default:
+        return res.json({ message: "Unhandled event type" });
+    }
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
